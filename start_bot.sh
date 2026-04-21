89057c77e6e2ecca02be3da4004713620ae6d05c
moekamel94 <mohammed.a.kamil@gmail.com>
2026-04-21 17:11:03 +0300

    Use venv python in startup scripts

--- start_bot.sh
+++ start_bot.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 while true; do
     echo "[$(date)] Starting bot..."
-    python3 main.py --discord
+    ~/venv/bin/python3 main.py --discord
     echo "[$(date)] Bot exited (code $?). Restarting in 15 seconds..."
     sleep 15
 done
